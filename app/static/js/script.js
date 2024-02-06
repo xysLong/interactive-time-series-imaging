c9e9c9feef2f562e9002f9ac4c0f0b7b87eeb077
Xingyu Long <alheimur.zh@gmail.com>
2024-02-06 21:33:35 +0100

    Warning for non-monotonicity

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,4 +1,6 @@
 let isDrawing = false;
+let currentX = -1;
+let currentY = -1;
 let coordinates = [];
 
 const canvas = document.getElementById('curveCanvas');
@@ -14,23 +16,45 @@ function startDrawing(e) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     isDrawing = true;
     coordinates = [];
-    coordinates.push({x: e.pageX - canvas.offsetLeft, y: e.pageY - canvas.offsetTop});
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'black';
+    currentX = e.pageX - canvas.offsetLeft;
+    currentY = e.pageY - canvas.offsetTop;
+    coordinates.push({x: currentX, y: currentY});
     ctx.beginPath();
-    ctx.moveTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+    ctx.moveTo(currentX, currentY);
 }
 
 function draw(e) {
     if (!isDrawing) return;
-    coordinates.push({x: e.pageX - canvas.offsetLeft, y: e.pageY - canvas.offsetTop});
-    ctx.lineTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
-    ctx.stroke();
+    const currentX_ = e.pageX - canvas.offsetLeft;
+    const currentY_ = e.pageY - canvas.offsetTop;
+    if (currentX_ !== currentX || Math.abs(currentY_ - currentY) >= 10) {
+        currentX = currentX_;
+        currentY = currentY_;
+        coordinates.push({x: currentX, y: currentY});
+        ctx.lineTo(currentX, currentY);
+        ctx.stroke();
+    }
 }
 
 function stopDrawing() {
     if (!isDrawing) return;
     ctx.closePath();
     isDrawing = false;
-    if (!isMonotonicallyIncreasing()) {
+    const nonIncreasingSequences = findNonIncreasingSequences();
+    if (nonIncreasingSequences.length > 0) {
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = 'coral';
+        for (const sequence of nonIncreasingSequences) {
+            ctx.beginPath();
+            ctx.moveTo(sequence[0].x, sequence[0].y);
+            for (let i = 1; i < sequence.length; i++) {
+                ctx.lineTo(sequence[i].x, sequence[i].y);
+            }
+            ctx.stroke();
+            ctx.closePath();
+        }
         alert('Warning: x values are not monotonically increasing!');
     } else {
         updatePlot();
@@ -75,13 +99,29 @@ function updatePlot() {
         });
 }
 
-function isMonotonicallyIncreasing() {
+
+function findNonIncreasingSequences() {
+    const result = [];
+    let sequence = [];
+
     for (let i = 1; i < coordinates.length; i++) {
-        if (coordinates[i].x < coordinates[i - 1].x) {
-            return false;
+        if (coordinates[i].x <= coordinates[i - 1].x) {
+            sequence.push(coordinates[i - 1]);
+        } else {
+            if (sequence.length > 0) {
+                sequence.push(coordinates[i - 1]);
+                result.push(sequence);
+                sequence = [];
+            }
         }
     }
-    return true;
+
+    if (sequence.length > 0) {
+        sequence.push(coordinates[coordinates.length - 1]);
+        result.push(sequence);
+    }
+
+    return result;
 }
 
 
@@ -91,12 +131,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const frequency = 0.05;
     const animationSpeed = 0.02;
 
+    function f(x) {
+        return centerY + Math.random() * amplitude * Math.sin(frequency * x);
+    }
+
     function initialize() {
         let startX = Math.random() * (canvas.width / 4);
         let endX = (3 * canvas.width / 4) + Math.random() * (canvas.width / 4);
-        let previousX = startX;
-        let previousY = centerY + amplitude * Math.sin(frequency * startX);
-        return [startX, endX, previousX, previousY, 0., 1.]
+        return [startX, endX, startX, f(startX), 0., 1.]
     }
 
     let [startX, endX, previousX, previousY, t, alpha] = initialize();
@@ -104,8 +146,14 @@ document.addEventListener("DOMContentLoaded", function () {
     let isAnimating = true;
 
     async function drawRandomStroke() {
+        if (t > 1.0) {
+            await delay(1000);
+            await fadeOut();
+            [startX, endX, previousX, previousY, t, alpha] = initialize();
+        }
+        t += animationSpeed;
         const currentX = startX + (endX - startX) * t;
-        const currentY = centerY + Math.random() * amplitude * Math.sin(frequency * currentX);
+        const currentY = f(currentX);
 
         ctx.beginPath();
         ctx.moveTo(previousX, previousY);
@@ -115,13 +163,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
         previousX = currentX;
         previousY = currentY;
-        t += animationSpeed;
-        if (t >= 1.0) {
-            await delay(1000);
-            await fadeOut();
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            [startX, endX, previousX, previousY, t, alpha] = initialize();
-        }
+
+
         if (isAnimating) {
             animationId = requestAnimationFrame(drawRandomStroke);
         }
@@ -143,6 +186,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 await delay(100);
             }
         }
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
 
     function delay(ms) {
